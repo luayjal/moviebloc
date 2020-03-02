e7132faabd463a2820559fd7f6531f3693825b2e
luayjal <49620453+luayjal@users.noreply.github.com>
2020-03-02 11:09:16 +0200

    moviebloc
    moviebloc

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var myBody = document.getElementById('myBody');
+var header = document.getElementById('header');
+var logo = document.getElementById('logo');
+
+
+
+//if(myBody.clientWidth <= 768)
+//{
+   
+  //  logo.parentNode.removeChild(logo);
+   
+//}
